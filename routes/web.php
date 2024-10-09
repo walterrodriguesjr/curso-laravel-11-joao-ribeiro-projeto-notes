9c09ef86d68f20475499cfa351b90fd98a8f34ef
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-09 14:58:00 -0300

    atualizando

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,3 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/about', function(){
-    echo 'about us';
-});
-
-Route::get('/main/{value}',[MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
