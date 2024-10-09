f39a4bd1776b0a95070cc7389adca6383ab30f22
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-09 15:09:01 -0300

    atualizando

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-
+//auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
