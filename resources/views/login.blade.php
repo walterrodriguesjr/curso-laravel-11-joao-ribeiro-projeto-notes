c5ef1e81955eb5375903a64a78dbf90594d3065f
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-11 17:26:57 -0300

    atualizando

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,17 +53,7 @@
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
 
-                    {{-- errors --}}
-                    @if ($errors->any())
-                        <div class="alert alert-danger mt-3">
-                            <ul class="m-0">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{$error}}</li>
-                                @endforeach
-                            </ul>
-                        </div>
 
-                    @endif
                 </div>
             </div>
         </div>
