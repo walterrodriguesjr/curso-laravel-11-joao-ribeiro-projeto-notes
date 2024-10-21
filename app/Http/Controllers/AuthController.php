57e11d540b530049347652bce14a2f03ce9ccc05
Walter Rodrigues Junior <walter.junior00@pm.pr.gov.br>
2024-10-21 13:36:54 -0300

    atualizando

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,10 +13,7 @@ class AuthController extends Controller
         return view('login');
     }
 
-    public function logout()
-    {
-        echo 'logout';
-    }
+    
 
     public function loginSubmit(Request $request) {
         //form validation
@@ -39,10 +36,44 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        //get all the users from the DataBase
-        $users = User::all()->toArray();
+        //check if user exists 
+        $user = User::where('username', $username)
+                    ->where('deleted_at', NULL)
+                    ->first();
 
-        return $users;
+       if(!$user){
+        return redirect()
+               ->back()
+               ->withInput()
+               ->with('loginError', 'Username ou password incorretos!');
+       }
+       //check if password is correct 
+       if(!password_verify($password, $user->password)){
+        return redirect()
+               ->back()
+               ->withInput()
+               ->with('loginError', 'Username ou password incorretos!');
+       }
+        //update last login
+       $user->last_login = date('Y-m-d H:i:s');
+       $user->save();
 
+       //login user 
+       session([
+        'user' => [
+            'id' => $user->id,
+            'username' => $user->username 
+        ]
+        ]);
+
+        echo 'LOGIN COM SUCESSO!';
+
+       }
+
+    
+
+    public function logout()
+    {
+        echo 'logout';
     }
 }
