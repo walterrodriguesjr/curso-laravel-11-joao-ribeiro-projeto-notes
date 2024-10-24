bd78f95031e9753311182608652b052be88fade8
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-24 13:27:47 -0300

    atualizando

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        echo 'inside the app!';
+    }
 
+    public function newNote()
+    {
+        echo 'creating new note!';
+    }
 }
