7f3a5396440f7eddfa5e1635efe71cf168855fa4
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-30 15:51:55 -0300

    atualizando

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        //load user's notes 
-
+        //load user's notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+        die();
         //show home view
         return view('home');
     }
--- app/User.php
+++ app/User.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
 
-    
-
+    //função que relaciona 1 User com muitos Notes
+    public function notes()
+    {
+        //User pode ter muito em Note, usando a coluna user_id de Note
+        return $this->hasMany(Note::class);
+    }
 }
