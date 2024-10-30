054e23c8229cfe4ae16bf98eeb0640ce21139c8a
walterrodriguesjr <walterrjr.86@gmail.com>
2024-10-30 16:41:38 -0300

    atualizando

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,12 +14,9 @@ class MainController extends Controller
         $id = session('user.id');
         $user = User::find($id)->toArray();
         $notes = User::find($id)->notes()->get()->toArray();
-        echo '<pre>';
-        print_r($user);
-        print_r($notes);
-        die();
+
         //show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
