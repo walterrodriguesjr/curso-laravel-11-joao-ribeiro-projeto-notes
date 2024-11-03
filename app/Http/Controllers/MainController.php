24a25b2fb775d58e897237b09d8051547abd9564
walterrodriguesjr <walterrjr.86@gmail.com>
2024-11-03 14:49:55 -0300

    atualizando

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Note;
 use App\Services\Operations;
 use App\User;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -30,9 +31,36 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-        $dados = $request->all();
-        dd($dados);
-        echo "Im creating a new note.";
+        //validate request
+        $request->validate(
+            //rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                //error message
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A nota é obrigatório',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        //get user id
+        $id = session('user.id');
+
+        //create new note
+        $note = New Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        //redirect to home
+        return redirect()->route('home');
     }
 
     public function editNote($id)
